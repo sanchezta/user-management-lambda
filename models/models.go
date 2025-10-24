d0a4405b0aceb12625f674b269b55bc44f964909
Adrian Sanchez <sanchezta@Mac.home>
2025-10-24 17:52:17 -0400

    feat(models): add SecretRDSJson and SignUp structs

---
+++ models/models.go
@@ -0,0 +1,21 @@
+package models
+
+
+type SecretRDSJson struct {
+
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	Engine		 string `json:"engine"`
+	Host 			 string `json:"host"`
+
+	Port			 int `json:"port"`
+	DbClusterIndentifier string `json:"dbClusterIndentifier"`
+
+}
+
+type SignUp struct {
+
+	UserEmail string `json:"UserEmail"`
+	UserUUID  string `json:"UserUUID"`
+
+}
