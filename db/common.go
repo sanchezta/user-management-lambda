bbab83140ecf4ee74eb4eb95297fa6b283c7cb1a
Adrian Sanchez <sanchezta@Mac.home>
2025-10-27 11:16:23 -0400

    feat(db): add MySQL connection package with secret integration

--- db/common.go
+++ db/common.go
@@ -1,8 +1,11 @@
 package db
 
 import (
+	"database/sql"
+	"fmt"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/sanchezta/user-management-lambda/models"
 	"github.com/sanchezta/user-management-lambda/secrets"
 )
@@ -10,10 +13,45 @@ import (
 
 var SecretModel models.SecretRDSJson
 var err error
+var Db *sql.DB
 
 func ReadScret () error{
 
 	SecretModel, err = secrets.GetSecret(os.Getenv("SecretName"))
-
+	
 	return  err
+}
+
+
+func DbConnect() error {
+    Db, err = sql.Open("mysql", ConnStr(SecretModel))
+    if err != nil {
+        fmt.Println(err.Error())
+        return err
+    }
+
+    err = Db.Ping()
+    if err != nil {
+        fmt.Println(err.Error())
+        return err
+    }
+
+    fmt.Println("Conexión a MySQL exitosa")
+    return nil
+}
+
+
+func ConnStr(keys models.SecretRDSJson) string{
+	var dbUser, authToken, dbEndpoint, dbName string
+
+	dbUser = keys.Username
+	authToken = keys.Password
+	dbEndpoint = keys.Host
+	dbName = "gambit"
+
+	dns := fmt.Sprintf("%s:%s@tcp(%s)/%s?allowCleartextPasswords=true",dbUser,authToken,dbEndpoint,dbName)
+
+	fmt.Println(dns)
+
+	return dns
 }
